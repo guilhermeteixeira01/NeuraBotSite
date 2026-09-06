c9e2fb385ce3f802889f5c4ec187d3cf48e9af6a
guilhermeteixeira01 <guilherme.teixeira00@outlook.com>
2026-09-06 15:13:04 -0300

    refactor: remover importação não utilizada e melhorar a formatação do código

--- deploy-prep.mjs
+++ deploy-prep.mjs
@@ -1,11 +1,15 @@
-import { cpSync, rmSync, mkdirSync, writeFileSync, cp } from 'fs'
+import { cpSync, rmSync, mkdirSync, writeFileSync } from 'fs'
 
 rmSync('dist', { recursive: true, force: true })
+
 mkdirSync('dist', { recursive: true })
+
 mkdirSync('dist/dashboard', { recursive: true })
 
 cpSync('NeuraBot/dist', 'dist', { recursive: true })
+
 cpSync('DashBoard/dist', 'dist/dashboard', { recursive: true })
+
 cpSync('GerarEmbed/dist', 'dist/GeradorEmbed', { recursive: true })
 
 writeFileSync('dist/CNAME', 'neurabot.com.br')
